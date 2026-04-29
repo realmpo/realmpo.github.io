409bd9f9ef9991d9457fe476f230e57880715d56
Meower-Pro <drvailshery@gmail.com>
2026-04-29 15:03:22 -0600

    Update indexCEH.js

--- indexCEH.js
+++ indexCEH.js
@@ -2,11 +2,21 @@
 let enContent = `
 <button class='ceh-translate-button' onclick='translateAll()'>Cetlennen hipu shid̃a boco? Kad̃e shid̃a benci.</button>
 <h1>Hi! This is my site!</h1>
+<section class='link-clicking-section'>
+  <h3>Click these links!</h3>
+  <a href='https://scratch.mit.edu/users/Meower-Pro'><button>Scratch profile</button></a>
+  <a href='https://conworkshop.com/view_language.php?l=CEHP'><button>Latin Catlanian page</button></a>
+</section>
 `;
 
 let cehContent = `
 <button class='ceh-translate-button' onclick='translateAll()'>Do you speak English? Click here.</button>
 <h1>Had̃a!</h1>
+<section class='link-clicking-section'>
+  <h3>D̃obis shid̃a benci!</h3>
+  <a href='https://scratch.mit.edu/users/Meower-Pro'><button>Lona o Scratch</button></a>
+  <a href='https://conworkshop.com/view_language.php?l=CEHP'><button>Lona o cetlennen hipu</button></a>
+</section>
 `;
 
 let clang = 'en';
